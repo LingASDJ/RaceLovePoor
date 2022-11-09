54d2c48e631fa0d0a8e347883fd4a1b62298f1b4
LingASDJ <2735951230@qq.com>
2022-11-09 10:51:59 +0800

    Revert "Revert "路由Demo，请不要合并到主分支""
    This reverts commit bed4cbc4cbfe409dba5f7bf14779b93cf3f781b4.

---
+++ src/app/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- 路由部署完毕 以下是Demo -->
+  <div id="app">
+    <!-- 重要的文件在router/index.js中 -->
+   <router-link to="/home">首页</router-link>
+    <router-link to="/about">关于</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+//引入scss文件
+@import "../scss/variable.scss";
+@import "../scss/public.scss";
+</style>
+
---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+      <h2>我是Home</h2>
+      <p>我是Home的页面内容</p>
+    </div>
+  </template>
+  
+  <script>
+  // 导出，然后在index.js中导入
+  export default {
+    name: 'Home'
+  }
+  </script>
+  
+  <style>
+  
+  </style>
