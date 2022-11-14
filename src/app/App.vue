7b7b21e805dd75b9ae279e14b630d32493000a2c
LingASDJ <2735951230@qq.com>
2022-11-14 14:12:20 +0800

    Update App.vue

--- src/app/App.vue
+++ src/app/App.vue
@@ -13,6 +13,7 @@
 // 引入页面
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
+
 export default {
     name: "App",
     //注册组件
@@ -20,7 +21,8 @@ export default {
         Header,
         Footer,
     },
-};
+  };
+
 </script>
 
 <style lang="scss">
