96ee624b219490bcd12191d7b121421e133c050f
PLee <101233711+pLeepl@users.noreply.github.com>
2022-11-14 23:28:38 +0800

    图片和代码提交

--- src/components/MidPass/index.vue
+++ src/components/MidPass/index.vue
@@ -4,7 +4,11 @@
     <div class="Toprotation">
       <div class="login">
         <div class="content">
-          <img class="tx" src="../../assets/images/login-png/头像.png" alt="" />
+          <img
+            class="tx"
+            src="../../assets/images/index/login-png/头像.png"
+            alt=""
+          />
           <h2>Hi! 你好</h2>
           <div class="button">
             <button class="login1">登录</button>
@@ -12,27 +16,39 @@
           </div>
           <div class="tbbox">
             <div class="tb">
-              <img src="../../assets/images/login-png/宝贝收藏.png" alt="" />
+              <img
+                src="../../assets/images/index/login-png/宝贝收藏.png"
+                alt=""
+              />
               <p>宝贝收藏</p>
             </div>
             <div class="tb">
-              <img src="../../assets/images/login-png/买过的店.png" alt="" />
+              <img
+                src="../../assets/images/index/login-png/买过的店.png"
+                alt=""
+              />
               <p>买过的店</p>
             </div>
             <div class="tb">
-              <img src="../../assets/images/login-png/收藏的店.png" alt="" />
+              <img
+                src="../../assets/images/index/login-png/收藏的店.png"
+                alt=""
+              />
               <p>收藏的店</p>
             </div>
             <div class="tb">
-              <img src="../../assets/images/login-png/我的足迹.png" alt="" />
+              <img
+                src="../../assets/images/index/login-png/我的足迹.png"
+                alt=""
+              />
               <p>我的足迹</p>
             </div>
           </div>
         </div>
       </div>
       <div class="banner">
-        <!-- <img src="../../assets/images/banner/02.jpg" alt="" /> -->
-        <img src="../../assets/images/banner/01.jpg" alt="" />
+        <!-- <img src="../../assets/images/index/banner/02.jpg" alt="" /> -->
+        <img src="../../assets/images/index/banner/01.jpg" alt="" />
       </div>
     </div>
     <!-- 热门商品部分 -->
@@ -51,15 +67,286 @@
           <span class="num second">00</span>
         </p>
       </div>
+      <!-- 商品 -->
+      <div class="right">
+        <div class="merchandise">
+          <div class="right_pro-l">
+            <div class="imgbox">
+              <img
+                src="../../assets/images/index/农产品图/水果/荔枝.jpg"
+                alt=""
+              />
+            </div>
+            <div class="right_pro_l_l">
+              <div class="right_pro-price1">￥20.6</div>
+              <div class="right_pro-price2">￥36.6</div>
+              <p>广东高州荔枝</p>
+              <span>精品</span>
+            </div>
+          </div>
+          <div class="right_pro-l">
+            <div class="imgbox">
+              <img
+                src="../../assets/images/index/农产品图/水果/荔枝.jpg"
+                alt=""
+              />
+            </div>
+            <div class="right_pro_l_l">
+              <div class="right_pro-price1">￥20.6</div>
+              <div class="right_pro-price2">￥36.6</div>
+              <p>广东高州荔枝</p>
+              <span>精品</span>
+            </div>
+          </div>
+          <div class="right_pro-l">
+            <div class="imgbox">
+              <img
+                src="../../assets/images/index/农产品图/水果/荔枝.jpg"
+                alt=""
+              />
+            </div>
+            <div class="right_pro_l_l">
+              <div class="right_pro-price1">￥20.6</div>
+              <div class="right_pro-price2">￥36.6</div>
+              <p>广东高州荔枝</p>
+              <span>精品</span>
+            </div>
+          </div>
+          <div class="right_pro-l">
+            <div class="imgbox">
+              <img
+                src="../../assets/images/index/农产品图/水果/荔枝.jpg"
+                alt=""
+              />
+            </div>
+            <div class="right_pro_l_l">
+              <div class="right_pro-price1">￥20.6</div>
+              <div class="right_pro-price2">￥36.6</div>
+              <p>广东高州荔枝</p>
+              <span>精品</span>
+            </div>
+          </div>
+          <div class="right_pro-l">
+            <div class="imgbox">
+              <img
+                src="../../assets/images/index/农产品图/水果/荔枝.jpg"
+                alt=""
+              />
+            </div>
+            <div class="right_pro_l_l">
+              <div class="right_pro-price1">￥20.6</div>
+              <div class="right_pro-price2">￥36.6</div>
+              <p>广东高州荔枝</p>
+              <span>精品</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- 天天特价 -->
+    <div class="Specialoffer">
+      <div class="right-img-3-1">
+        <div class="img-3">
+          <img src="../../assets/images/index/农产品图/农副/枸杞.jpg" />
+        </div>
+        <div class="img-nuber">
+          <p class="nuber-1">￥<span>30.00</span>元/斤</p>
+          <p class="nuber-2">50斤起批</p>
+        </div>
+        <div class="img-text">
+          <p>长岛淡晒海米金钩批发</p>
+        </div>
+        <div class="img-text-1">
+          <p class="it-1">库存：2000斤</p>
+          <p class="it-2">经销批发</p>
+        </div>
+      </div>
+      <div class="right-img-3-1">
+        <div class="img-3">
+          <img src="../../assets/images/index/农产品图/农副/枸杞.jpg" />
+        </div>
+        <div class="img-nuber">
+          <p class="nuber-1">￥<span>30.00</span>元/斤</p>
+          <p class="nuber-2">50斤起批</p>
+        </div>
+        <div class="img-text">
+          <p>长岛淡晒海米金钩批发</p>
+        </div>
+        <div class="img-text-1">
+          <p class="it-1">库存：2000斤</p>
+          <p class="it-2">经销批发</p>
+        </div>
+      </div>
+      <div class="right-img-3-1">
+        <div class="img-3">
+          <img src="../../assets/images/index/农产品图/农副/枸杞.jpg" />
+        </div>
+        <div class="img-nuber">
+          <p class="nuber-1">￥<span>30.00</span>元/斤</p>
+          <p class="nuber-2">50斤起批</p>
+        </div>
+        <div class="img-text">
+          <p>长岛淡晒海米金钩批发</p>
+        </div>
+        <div class="img-text-1">
+          <p class="it-1">库存：2000斤</p>
+          <p class="it-2">经销批发</p>
+        </div>
+      </div>
+      <div class="right-img-3-1">
+        <div class="img-3">
+          <img src="../../assets/images/index/农产品图/农副/枸杞.jpg" />
+        </div>
+        <div class="img-nuber">
+          <p class="nuber-1">￥<span>30.00</span>元/斤</p>
+          <p class="nuber-2">50斤起批</p>
+        </div>
+        <div class="img-text">
+          <p>长岛淡晒海米金钩批发</p>
+        </div>
+        <div class="img-text-1">
+          <p class="it-1">库存：2000斤</p>
+          <p class="it-2">经销批发</p>
+        </div>
+      </div>
+      <div class="right-img-3-1">
+        <div class="img-3">
+          <img src="../../assets/images/index/农产品图/农副/枸杞.jpg" />
+        </div>
+        <div class="img-nuber">
+          <p class="nuber-1">￥<span>30.00</span>元/斤</p>
+          <p class="nuber-2">50斤起批</p>
+        </div>
+        <div class="img-text">
+          <p>长岛淡晒海米金钩批发</p>
+        </div>
+        <div class="img-text-1">
+          <p class="it-1">库存：2000斤</p>
+          <p class="it-2">经销批发</p>
+        </div>
+      </div>
+      <div class="right-img-3-1">
+        <div class="img-3">
+          <img src="../../assets/images/index/农产品图/农副/枸杞.jpg" />
+        </div>
+        <div class="img-nuber">
+          <p class="nuber-1">￥<span>30.00</span>元/斤</p>
+          <p class="nuber-2">50斤起批</p>
+        </div>
+        <div class="img-text">
+          <p>长岛淡晒海米金钩批发</p>
+        </div>
+        <div class="img-text-1">
+          <p class="it-1">库存：2000斤</p>
+          <p class="it-2">经销批发</p>
+        </div>
+      </div>
+      <div class="right-img-3-1">
+        <div class="img-3">
+          <img src="../../assets/images/index/农产品图/农副/枸杞.jpg" />
+        </div>
+        <div class="img-nuber">
+          <p class="nuber-1">￥<span>30.00</span>元/斤</p>
+          <p class="nuber-2">50斤起批</p>
+        </div>
+        <div class="img-text">
+          <p>长岛淡晒海米金钩批发</p>
+        </div>
+        <div class="img-text-1">
+          <p class="it-1">库存：2000斤</p>
+          <p class="it-2">经销批发</p>
+        </div>
+      </div>
+      <div class="right-img-3-1">
+        <div class="img-3">
+          <img src="../../assets/images/index/农产品图/农副/枸杞.jpg" />
+        </div>
+        <div class="img-nuber">
+          <p class="nuber-1">￥<span>30.00</span>元/斤</p>
+          <p class="nuber-2">50斤起批</p>
+        </div>
+        <div class="img-text">
+          <p>长岛淡晒海米金钩批发</p>
+        </div>
+        <div class="img-text-1">
+          <p class="it-1">库存：2000斤</p>
+          <p class="it-2">经销批发</p>
+        </div>
+      </div>
+      <div class="right-img-3-1">
+        <div class="img-3">
+          <img src="../../assets/images/index/农产品图/农副/枸杞.jpg" />
+        </div>
+        <div class="img-nuber">
+          <p class="nuber-1">￥<span>30.00</span>元/斤</p>
+          <p class="nuber-2">50斤起批</p>
+        </div>
+        <div class="img-text">
+          <p>长岛淡晒海米金钩批发</p>
+        </div>
+        <div class="img-text-1">
+          <p class="it-1">库存：2000斤</p>
+          <p class="it-2">经销批发</p>
+        </div>
+      </div>
+      <div class="right-img-3-1">
+        <div class="img-3">
+          <img src="../../assets/images/index/农产品图/农副/枸杞.jpg" />
+        </div>
+        <div class="img-nuber">
+          <p class="nuber-1">￥<span>30.00</span>元/斤</p>
+          <p class="nuber-2">50斤起批</p>
+        </div>
+        <div class="img-text">
+          <p>长岛淡晒海米金钩批发</p>
+        </div>
+        <div class="img-text-1">
+          <p class="it-1">库存：2000斤</p>
+          <p class="it-2">经销批发</p>
+        </div>
+      </div>
+      <div class="right-img-3-1">
+        <div class="img-3">
+          <img src="../../assets/images/index/农产品图/农副/枸杞.jpg" />
+        </div>
+        <div class="img-nuber">
+          <p class="nuber-1">￥<span>30.00</span>元/斤</p>
+          <p class="nuber-2">50斤起批</p>
+        </div>
+        <div class="img-text">
+          <p>长岛淡晒海米金钩批发</p>
+        </div>
+        <div class="img-text-1">
+          <p class="it-1">库存：2000斤</p>
+          <p class="it-2">经销批发</p>
+        </div>
+      </div>
+      <div class="right-img-3-1">
+        <div class="img-3">
+          <img src="../../assets/images/index/农产品图/农副/枸杞.jpg" />
+        </div>
+        <div class="img-nuber">
+          <p class="nuber-1">￥<span>30.00</span>元/斤</p>
+          <p class="nuber-2">50斤起批</p>
+        </div>
+        <div class="img-text">
+          <p>长岛淡晒海米金钩批发</p>
+        </div>
+        <div class="img-text-1">
+          <p class="it-1">库存：2000斤</p>
+          <p class="it-2">经销批发</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <style>
 /* @import "../scss/variable.scss"; */
 /* @import "../../scss/public.scss"; */
-.app{
+.app {
   background: #f5f5f5;
 }
+
 /* 顶部轮播图部分 */
 .Toprotation {
   display: flex;
@@ -68,26 +355,31 @@
   margin: 0 auto;
   padding-bottom: 20px;
 }
+
 .login {
   width: 380px;
   height: 400px;
   display: flex;
   flex-direction: column;
 }
+
 .login h2 {
   text-align: center;
   padding-top: 32px;
 }
+
 .content {
   display: flex;
   flex-direction: column;
   padding-top: 60px;
 }
+
 .tx {
   width: 60px;
   height: 60px;
   margin: 0 auto;
 }
+
 .button {
   display: flex;
   margin: 0 auto;
@@ -103,73 +395,195 @@
   color: aliceblue;
   margin: 0 4px;
 }
+
 .login1 {
   background: #ff5400;
 }
+
 .register1 {
   background: #ff7f00;
 }
+
 .tbbox {
   display: flex;
   justify-content: space-evenly;
   padding-top: 32px;
 }
+
 .tb {
   width: 56px;
   height: 48px;
   display: flex;
   flex-direction: column;
 }
+
 .tb img {
   width: 26px;
   height: 26px;
   margin: 0 auto;
 }
+
 .tb p {
   width: 56px;
   text-align: center;
   size: 12px !important;
   padding-top: 10px;
 }
+
 .banner {
   width: 800px;
   height: 400px;
 }
+
 .banner img {
   width: 800px;
   height: 400px;
 }
+
 /* 热门商品部分 */
-.hot-secondkill{
-width: 1200px;
-margin: 0 auto;
+.hot-secondkill {
+  width: 1200px;
+  margin: 0 auto;
+  padding-bottom: 20px;
 }
+
 /* 1.秒杀 */
 .left {
-  height: 290px;
+  height: 240px;
   float: left;
-  background-image: url(../../assets/images/ms.png);
+  background-image: url(../../assets/images/index/ms.png);
   background-position: center center;
 }
-  h4{
-    height: 114px;
-    line-height: 114px;
-    font-size: 36px;
-    color: white;
-    text-align: center;
-    font-family: "黑体";
-    margin-bottom: 74px;
-  }
-  .text{
-    color: white;
-    text-align: center;
-    font-size: 20px;
-    margin-bottom: 20px;
-  }
-  .time{
-    text-align: center;
-    color: white;
-    font-size: 24px;
-    width: 220px;
-  }
+
+h4 {
+  height: 80px;
+  line-height: 80px;
+  font-size: 36px;
+  color: white;
+  text-align: center;
+  font-family: "黑体";
+  margin-bottom: 74px;
+}
+
+.text {
+  color: white;
+  text-align: center;
+  font-size: 20px;
+  margin-bottom: 20px;
+}
+
+.time {
+  text-align: center;
+  color: white;
+  font-size: 24px;
+  width: 220px;
+}
+
+/* 2.商品 */
+.right_pro-l {
+  background-color: rgb(230, 230, 230);
+  width: 190px;
+  height: 240px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.imgbox {
+  width: 160px;
+  height: 160px;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 10px;
+}
+
+.imgbox img {
+  width: 100%;
+  transition: all 1s;
+}
+
+.right_pro-price1 {
+  float: left;
+  font-size: 22px;
+  font-weight: normal;
+  color: #e40c61;
+}
+
+.right_pro-price2 {
+  font-size: 12px;
+  letter-spacing: 1px;
+  color: #a09e9e;
+  text-decoration: line-through;
+}
+
+.right_pro_l_l p {
+  display: block;
+  float: left;
+  width: 100px;
+  height: 24px;
+  font-size: 12px;
+  color: #666;
+}
+
+.right_pro_l_l span {
+  display: inline-block;
+  font-size: 14px;
+  padding: 3px 6px;
+  color: rgb(235, 235, 235);
+  background-color: #3a880e;
+  margin-right: 10px;
+}
+
+.merchandise {
+  display: flex;
+  justify-content: space-evenly;
+}
+
+/* 天天特价 */
+.Specialoffer {
+  width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+}
+.right-img-3-1 {
+  width: 200px;
+  height: 270px;
+  background-color: #ffffff;
+  padding-top: 5px;
+  margin-bottom: 10px;
+}
+.img-3 img {
+  width: 170px;
+  height: 170px;
+}
+.right-img-3-1 .img-3 {
+  margin: 0 12px;
+}
+
+.img-nuber {
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  align-items: baseline;
+  padding: 0 10px;
+  margin-bottom: 5px;
+}
+
+.img-text {
+  display: flex;
+  justify-content: center;
+  font-size: 14px;
+  color: #404040;
+  margin-bottom: 8px;
+}
+
+.img-text-1 {
+  display: flex;
+  justify-content: space-between;
+  padding: 0 13px;
+}
 </style>
