e0919b473b77ac9722f11a9edb2bd9176cfe4432
PLee <101233711+pLeepl@users.noreply.github.com>
2022-11-15 11:39:50 +0800

    代码提交

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -47,7 +47,7 @@
           </li>
           <li class="fl"><i class="shortcut_s"></i></li>
           <li class="fl">
-            <div class="menu_hd"><a href="#">商家入驻</a></div>
+            <div class="menu_hd"><a href="#">农户认证</a></div>
           </li>
           <li class="fl"><i class="shortcut_s"></i></li>
           <li class="fl">
@@ -79,16 +79,8 @@
         </div>
         <div class="header_searchbox">
           <form action="#">
-            <input
-              name="search"
-              type="text"
-              class="header_search_input"
-              default_val="锡货网三期上线全场五折"
-              autocomplete="off"
-              x-webkit-speech=""
-              x-webkit-grammar="builtin:search"
-              lang="zh"
-            />
+            <input name="search" type="text" class="header_search_input" default_val="锡货网三期上线全场五折" autocomplete="off"
+              x-webkit-speech="" x-webkit-grammar="builtin:search" lang="zh" />
             <button type="submit" class="header_search_btn">搜索</button>
           </form>
           <ul class="hot_word">
@@ -102,21 +94,35 @@
         <div id="cart_box" class="cart_box">
           <a id="cart" class="cart_link" href="#" rel="nofollow">
             <a @click="CardCatLink"><span class="text">去购物车结算</span></a>
-            <img
-              src="../../assets/images/shopping_icon.png"
-              width="24"
-              height="24"
-              class="cart_gif"
-            />
+            <img src="../../assets/images/shopping_icon.png" width="24" height="24" class="cart_gif" />
 
             <!-- 购物车没有物品时，隐藏此num -->
             <!-- <span class="num">12</span>
                 <s class="icon_arrow_right"></s> -->
           </a>
         </div>
+        <div class="initio-1">
+          <ul>
+            <li>
+              <a href="#">首页</a>
+            </li>
+            <li>
+              <a href="./purchasing.html">采购大厅</a>
+            </li>
+            <li>
+              <a href="./helpsFarmers.html">爱心助农</a>
+            </li>
+            <li>
+              <a href="./Information.html">农业资讯</a>
+            </li>
+            <li>
+              <a href="./about.html">关于我们</a>
+            </li>
+            <hr>
+          </ul>
+        </div>
         <span class="clr"></span>
       </div>
-      <hr />
     </div>
     <router-view></router-view>
   </div>
@@ -124,6 +130,7 @@
 </template>
 
 <script>
+<<<<<<< Updated upstream
 // 导出，然后在index.js中导入
 export default {
   name: "Home",
@@ -139,3 +146,22 @@ export default {
   }
 };
 </script>
+=======
+  // 导出，然后在index.js中导入
+  export default {
+    name: "Home",
+    props: {
+      appimg: {
+        default: "/src/assets/images/top_phone_icon.png"
+      }
+    },
+    methods: {
+      CardCatLink() {
+        this.$router.replace('/cardcat')
+      }
+    },
+    //引入ReloadAPP
+    inject: ['reloadApp']
+  };
+</script>
+>>>>>>> Stashed changes
